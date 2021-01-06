72820ba08ca7e260811cd0e8ffd93e423724588f
couettou <delignyb.pro@gmail.com>
2021-01-06 12:08:09 +0100

    feat(user): creation of the users module

---
+++ src/users/users.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      username: 'john',
+      password: 'changeme',
+      email: 'john@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'maria',
+      password: 'guess',
+      email: 'maria@hotmail.com',
+    },
+  ];
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  /* findOne(id: number) {
+    return `This action returns a #${id} user`;
+  } */
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
