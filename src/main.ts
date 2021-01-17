8e28064c0ad1d5b59bceac03aaf3bc204009e2ec
couettou <delignyb.pro@gmail.com>
2021-01-17 16:27:21 +0100

    feat(global): add helmet protection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as csurf from 'csurf';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(csurf());
+  app.use(csurf()); // ! Protection against exploit (unauthorized commands)
+  app.use(helmet()); // ! Protection against HTTP vulnerabilities
   await app.listen(3000);
 }
 bootstrap();
